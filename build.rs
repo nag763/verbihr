dc5219f0e4354bb9ecffb844ce8b87cbb124a133
nag763 <loic.labeye@tutanota.de>
2023-11-22 22:37:09 +0100

    Add some steps to the build.rs

--- build.rs
+++ build.rs
@@ -1,13 +1,19 @@
-use std::{collections::HashMap, fs::File};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    process::Command,
+};
 
 use serde::{Deserialize, Serialize};
 use std::io::prelude::*;
 
-const TRANSLATIONS_OUT_PATH : &str = "src/resources/translation.pc";
+const TRANSLATIONS_OUT_PATH: &str = "src/resources/translation.pc";
+const ASSETS_PATH: &str = "assets";
+const STYLES_PATH: &str = "assets/tailwind.css";
 
 #[derive(Deserialize)]
 struct LocaleDocumentRoot {
-    pub locales: Vec<Locale>
+    pub locales: Vec<Locale>,
 }
 
 #[derive(Deserialize, PartialEq, Eq, Clone, Serialize)]
@@ -16,7 +22,7 @@ struct Locale {
     short_name: String,
     navigator_names: Vec<String>,
     is_default: bool,
-    translations: TranslationMap
+    translations: TranslationMap,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -30,21 +36,52 @@ struct Verb {
     konjuktiv_ii_ich: String,
     imperativ_singular: String,
     imperativ_plural: String,
-    hilfsverb: String
+    hilfsverb: String,
 }
 
-
-#[derive(Debug,Clone, PartialEq, Eq, Deserialize, Serialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct TranslationMap(HashMap<String, String>);
 
 fn main() -> anyhow::Result<(), anyhow::Error> {
-    println!("cargo:rerun-if-changed=translation.yaml");
-    let translations_yaml: LocaleDocumentRoot = serde_yaml::from_str(include_str!("translation.yaml"))?;
-    let translations_pc = postcard::to_stdvec(&translations_yaml.locales)?;
-    let mut trans_file = match File::create(TRANSLATIONS_OUT_PATH) {
-        Ok(f) => f,
-        Err(_) => File::create(TRANSLATIONS_OUT_PATH)?,
+    let must_check_assets = {
+        if let Some(profile) = std::env::var("PROFILE").ok() {
+            if profile == "release" {
+                false
+            } else {
+                true
+            }
+        } else {
+            true
+        }
     };
-    trans_file.write_all(&translations_pc)?;
+    if must_check_assets {
+        if fs::metadata(ASSETS_PATH).is_err() {
+            fs::create_dir(ASSETS_PATH)?;
+        }
+        if fs::metadata(STYLES_PATH).is_err() {
+            Command::new("npx")
+                .args([
+                    "tailwindcss@3.3.0",
+                    "-i",
+                    "main.css",
+                    "-o",
+                    "assets/tailwind.css",
+                    "-c",
+                    "tailwind.config.js",
+                ])
+                .output()?;
+        }
+    }
+    println!("cargo:rerun-if-changed=src/resources/translation.yaml");
+    let translations_yaml: LocaleDocumentRoot =
+        serde_yaml::from_str(include_str!("src/resources/translation.yaml"))?;
+    if fs::metadata(TRANSLATIONS_OUT_PATH).is_err() {
+        let translations_pc = postcard::to_stdvec(&translations_yaml.locales)?;
+        let mut trans_file = match File::create(TRANSLATIONS_OUT_PATH) {
+            Ok(f) => f,
+            Err(_) => File::create(TRANSLATIONS_OUT_PATH)?,
+        };
+        trans_file.write_all(&translations_pc)?;
+    }
     Ok(())
 }
