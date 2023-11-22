f8e01916bca0dd1fa8a7b3f467038a2c4738aa4e
nag763 <loic.labeye@tutanota.de>
2023-11-22 23:42:00 +0100

    Responsive + main page start

--- src/components/body.rs
+++ src/components/body.rs
@@ -1,13 +1,31 @@
-use yew::{function_component, Html, html};
+use std::rc::Rc;
+
+use yew::{function_component, Html, html, use_context};
+
+use crate::{i18n::I18N, context::Context};
 
 #[function_component(Body)]
 pub fn body() -> Html {
-    html! {
-        <main class="flex-1 flex items-center justify-center text-black dark:text-white p-6 ">
-          <div class="text-center">
-            <h1 class="text-4xl font-bold mb-4">{"Willkomen"}</h1>
-            <p class="text-lg">{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit est eu justo sodales..."}</p>
-          </div>
-        </main>
-    }
+  let context = use_context::<Rc<Context>>().unwrap();
+  let translations = &context.translations;
+
+  html! {
+      <main class="grid grid-cols-9 items-center justify-center text-black dark:text-white p-6 md:p-12 h-full">
+        <div class="col-span-1 md:col-span-2"/>
+        <div class="flex flex-col items-center justify-center text-center col-span-7 md:col-span-5 h-full">
+          <h1 class="text-2xl md:text-4xl font-bold mb-4">{"Willkomen!"}</h1>
+          <p class="text-md md:text-lg"><I18N label={"intro"} {translations}/></p>
+          <div class="flex-grow"></div>
+          <button class="bg-blue-500 text-white py-4 px-8 rounded-lg flex items-center justify-center space-x-2 hover:bg-blue-700 transition duration-300 w-2/3 md:w-1/3 h-1/6">
+            <span><I18N label={"start"} {translations}/></span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+            </svg>
+          </button>
+          <div class="flex-grow"></div>
+
+        </div>
+        <div class="col-span-1 md:col-span-2"/>
+      </main>
+  }
 }
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -10,15 +10,15 @@ pub mod footer {
         let context = use_context::<Rc<Context>>().unwrap();
         let translations = &context.translations;
         html! {
-            <footer class="text-center sm:py-2 md:py-4 bg-slate-200 dark:bg-gray-900 h-full text-black dark:text-white text-xs ">
-            <div class="flex md:flex-col items-center justify-center sm:space-x-2 mdspace-x-4 mt-auto">
+            <footer class="text-center py-1 lg:py-2 bg-slate-200 dark:bg-gray-900 h-full text-black dark:text-white text-xs h-full ">
+            <div class="flex lg:flex-col items-center justify-center space-x-2 mt-auto">
                 <a href="https://github.com/nag763/verbihr" class="transform transition-transform duration-300 hover:scale-110" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path d="M12 0C5.373 0 0 5.373 0 12c0 5.303 3.438 9.8 8.207 11.387.6.11.793-.258.793-.577 0-.285-.012-1.04-.018-2.04-3.22.702-3.89-1.54-3.89-1.54-.525-1.327-1.282-1.68-1.282-1.68-1.048-.715.08-.702.08-.702 1.16.082 1.773 1.2 1.773 1.2 1.033 1.77 2.713 1.258 3.37.96.105-.748.405-1.26.737-1.546-2.586-.294-5.297-1.293-5.297-5.74 0-1.27.45-2.312 1.2-3.126-.12-.296-.522-1.482.114-3.08 0 0 1.008-.312 3.3 1.2a11.115 11.115 0 012.947-.4c1.002.007 2.007.135 2.947.4 2.29-1.512 3.297-1.2 3.297-1.2.636 1.598.234 2.784.114 3.08.75.814 1.2 1.856 1.2 3.126 0 4.458-2.715 5.442-5.305 5.728.42.36.795 1.068.795 2.15 0 1.55-.015 2.8-.015 3.18 0 .318.21.694.8.576C20.568 21.797 24 16.3 24 12c0-6.627-5.373-12-12-12z"/>
                     </svg>
                 </a>
                 <p>{"© 2023 LABEYE Loïc. "}<I18N label="all_rights_reserved" {translations} /></p>
-                <p class="sm:invisible md:visible"><I18N label="footer" {translations} /> </p>
+                <p class="hidden lg:block"><I18N label="footer" {translations} /> </p>
               </div>
             </footer>
         }
@@ -91,7 +91,7 @@ pub mod header {
         };
 
         html! {
-            <nav class="flex items-center justify-between p-6">
+            <nav class="flex items-center justify-between px-2 sm:px-4 md:px-6 h-py-1 sm:py-2 md:py-4 h-full">
             <div class="text-white font-bold text-xl">{"Verbihr"}</div>
             <div class="flex space-x-4">
             <div {onclick} class="w-6 h-6 text-black transform transition-transform duration-300 hover:rotate-180">
